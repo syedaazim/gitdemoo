b620bb07861881d92407539eea5b26a6cd440fb2
Aazim <srq338@gmail.com>
2020-12-06 19:24:26 +0530

    index.js is updated

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('./src/db/con');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+
+});
